e1a474a7d232540c4953fecb5b99cf8db7731e2c
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-09 09:40:21 -0400

    added ngrx dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { ShoppingReducer } from './store/reducers/shopping.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +15,7 @@ import { ShoppingReducer } from './store/reducers/shopping.reducer';
     StoreModule.forRoot({
       shopping: ShoppingReducer,
     }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
