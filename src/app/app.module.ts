861d1e243d1424762fadd8096a327757930715a7
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-09 09:31:56 -0400

    completed untill add item to shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { ShoppingReducer } from './store/reducers/shopping.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    FormsModule,
+    StoreModule.forRoot({
+      shopping: ShoppingReducer,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
